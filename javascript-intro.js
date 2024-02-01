d728b366bc20385348f1e869ba66fffed2b244ab
Seth Crist <sethcrist50@gmail.com>
2024-01-31 19:23:51 -0600

    added an html, and finished the first section of the questions

---
+++ javascript-intro.js
@@ -0,0 +1,46 @@
+// 1. Write the code that will log the output of 34 added to 71.
+console.log(34 + 71)
+
+// 2. Write the code that will log the output of 67 subtracted from 123.
+console.log(67 - 123)
+
+// 3. Write the code that will log the output of 56 multiplied by 23.
+console.log(56 * 23)
+
+// 4. Write the code that will log the output of 45 divided by 5.
+console.log(45 / 5)
+
+// 5. Write the code that will log the output of 5 to the power of 7.
+console.log(5 ** 7)
+
+// 6. Write the code that will log the whole number remainder of 33 divided by 6.
+console.log(33 % 6)
+
+// 7. Write the code that will log the length of a string containing your name
+let name = "seth"
+console.log(name.length)
+
+// 8. Write the code that will log whether your string includes the letter "e"?
+console.log(name.includes("e"))
+
+// 9. Write the code that will log the character at the first index of the string.
+console.log(name.charAt(0))
+
+// 10. Write the code that will log the string in all uppercase letters.
+console.log(name.toUpperCase())
+
+const theQuestion = "life, the universe, and everything"
+const theAnswer = 42
+
+// 11. Write the code that will log theAnswer divided by 2.
+// 12. Write the code that will log the whole number remainder of theAnswer when divided by 3.
+// 13. Write the code that will log the length of theQuestion.
+// 14. Write the code that will log the length of theQuestion divided by 2.
+// 15. Write the code that will log the length of theQuestion added to theAnswer.
+// 16. Write the code that will log the index of the character "f" in the theQuestion.
+// 17. Write the code that will log the index of the second comma in the theQuestion.
+// 18. Write the code that will log the concatenation of the two variables.
+// 19. Write the code that will log the character "l" from theQuestion.
+// 20. Write the code that will extract "the universe" from theQuestion.
+// 21. Write the code that will extract "everything" from theQuestion.
+// 22. Write the code that will log the last character of theQuestion without manually counting the number of characters.
