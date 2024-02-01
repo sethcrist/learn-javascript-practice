8fca855f7c3ad0f2c223d460113aa00900c4ee37
Seth Crist <sethcrist50@gmail.com>
2024-02-01 13:44:29 -0600

    first push

---
+++ conditionals.js
@@ -0,0 +1,33 @@
+//Write the code that will log true or false for the following:
+
+
+// 1. Is 34 divided by 3 greater than 67 divided by 2?
+// 2. Does 5 evaluate to the same as "5"?
+// 3. Does 5 strictly equal "5"?
+// 4. Does !3 strictly equal 3?
+// 5. Does "LEARN".length strictly equal 5 AND "Student".length strictly equal 7?
+// 6. Does "LEARN".length strictly equal 5 OR "Student".length strictly equal 10?
+// 7. Does "LEARN" contain the subset "RN"?
+// 8. Does "LEARN" contain the subset "rn"?
+// 9. Does "LEARN"[0] strictly equal "l"?
+// 10. Modify the code from the previous question to return true.
+
+// Write a conditional statement for the following prompts. Make sure you try different options and change the variables to ensure properly
+// working code.
+
+// 11. Write a statement that takes a variable of item and logs "in budget" if a price is $100 or less.
+// 12. Write a statement that takes a variable of hungry and logs "eat food" if you are hungry and "keep coding" if you are not hungry.
+// 13. Write a statement that takes a variable of trafficLight and logs "go" if the light is green, "slow down" if the light is yellow and "stop" if the light is red.
+// 14. Write a statement that takes two variables that are numbers and outputs the larger number. If the numbers are equal, output "the numbers are the same".
+// 15. Write a statement that takes a variable of a number and logs whether the number is odd, even, or zero.
+
+// stretch goals
+// Write a conditional statement for the following prompts.
+
+let num1 = 5
+let num2 = 10
+if (num1 > num2) {
+    console.log(`${num1} is less than ${num2}`);
+}
+console.log("hello")
+
