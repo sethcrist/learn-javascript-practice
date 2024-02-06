cef82c8dec4371af6656da296ecc6d938e263c65
Seth Crist <sethcrist50@gmail.com>
2024-02-05 18:53:08 -0600

    finished the exercise

--- functions.js
+++ functions.js
@@ -32,12 +32,60 @@
     // }
     // console.log(multiply(5, 5));
 
-// 6. Write a function named divisibleBy that takes two numbers as arguments and returns whether the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+// 6. Write a function named divisibleBy that takes two numbers as arguments and returns whether
+// the first number is evenly divisible by the second so that divisibleBy(10, 5) logs "10 is evenly divisible by 5".
+    // const divisibleBy = (num1, num2) => {
+    //     if (num1 % num2 === 0) {
+    //         return `${num1} and ${num2} is evenly divisible`
+    //     } else {
+    //         return `${num1} and ${num2} is not evely divisible`
+    //     }
+    // }
+    // console.log(divisibleBy(10, 6));
 
 // 7. Write a function named assignGrade that takes a number score as an argument and returns the letter grade for the score.
+    // const assignGrade = (num) => {
+    //     if (num >= 90) {
+    //         return "you have an A"
+    //     } else if (num >= 80) {
+    //         return "you have a B"
+    //     } else if (num >= 70) {
+    //         return "you have a C"
+    //     } else if (num >= 60) {
+    //         return "your are getting very close to failing"
+    //     } else {
+    //         return "you are failing my guy"
+    //     }
+    // }
+    // console.log(assignGrade(80))
 
 // 8. Write a function named isLonger that takes two strings as arguments and returns the string that contains the most characters.
+    // const isLonger = (str1, str2) => {
+    //     if (str1.length > str2.length) {
+    //         return `${str1} has more characters than ${str2}`
+    //     } else if (str1.length < str2.length){
+    //         return `${str1} has less characters than ${str2}`
+    //     } else {
+    //         return `${str1} has the same characters as ${str2}`
+    //     }
+    // }
+    // console.log(isLonger("yellow", "red"));
 
 // 9. Write a function named greaterNum that takes two numbers as arguments and returns whichever number is the greater (higher) number.
+    // const greaterNum = (num1, num2) => {
+    //     if (num1 > num2) {
+    //         return `${num1} is greater than ${num2}`
+    //     } else if (num1 < num2) {
+    //         return `${num1} is less than ${num2}`
+    //     } else {
+    //         return `${num1} and ${num2} are the same number`
+    //     }
+    // }
+    // console.log(greaterNum(5, 5));
 
 // 10. Write a function named yelling that takes a string as an argument and return the string in all uppercase case letters.
+    // const yelling = (str) => {
+    //     return str.toUpperCase()
+    // }
+    // console.log(yelling("hello, my name is seth"));
+
