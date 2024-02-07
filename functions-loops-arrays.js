9a757e37d0e38e2a5268cd2ce4ce3c15437d7834
Seth Crist <sethcrist50@gmail.com>
2024-02-07 10:50:58 -0600

    initial push

---
+++ functions-loops-arrays.js
@@ -0,0 +1,39 @@
+// 1. Write a function that takes in an array of numbers and returns an array with all numbers multiplied by 3.
+const testArr1 = [3, 9, 15, 4, 10]
+// output: [9, 27, 45, 12, 30]
+
+// 2. Write a function that takes in an array of numbers and returns a new array with only odd numbers.
+const testArr2 = [0, 2, -7, 3, 5, 8, 10, 13]
+// output: [-7, 3, 5, 13]
+
+// 3. Write a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: use the typeof method.
+    const comboArr = [
+    7,
+    "n",
+    true,
+    "i",
+    "c",
+    10,
+    "e",
+    -388,
+    "w",
+    3,
+    "o",
+    0,
+    "r",
+    false,
+    "k"
+]
+// output: "nicework"
+
+// 4. Create a function that takes in an array of numbers and returns the sum.
+const addThese1 = [1, 2, 3, 4]
+// output: 10
+
+const addThese2 = []
+// output: 0
+
+// 5. Create a function that takes in an array of numbers and returns the index of the largest number.
+// const indexHighestNumber = [1, 4, 2, 3]
+// // output: 1
+
