aebdf0487a8ca01cbe6d323a687b1ef877cec57f
Seth Crist <sethcrist50@gmail.com>
2024-02-20 11:09:44 -0600

    practice

--- arrays.js
+++ arrays.js
@@ -163,29 +163,51 @@ const planets = [
  * Read each console log below, and write some javascript code to perform
  * the step that it describes
  */
-planets.unshift("Sun")
-console.log('Adding "The Sun" to the beginning of the planets array.');
-console.log(planets);
+// planets.unshift("Sun")
+// console.log('Adding "The Sun" to the beginning of the planets array.');
+// console.log(planets);
 
-planets.push("Pluto")
-console.log('Adding "Pluto" to the end of the planets array.');
-console.log(planets);
+// planets.push("Pluto")
+// console.log('Adding "Pluto" to the end of the planets array.');
+// console.log(planets);
 
-planets.shift()
-console.log('Removing "The Sun" from the beginning of the planets array.');
-console.log(planets);
+// planets.shift()
+// console.log('Removing "The Sun" from the beginning of the planets array.');
+// console.log(planets);
 
-planets.pop()
-console.log('Removing "Pluto" from the end of the planets array.');
-console.log(planets);
+// planets.pop()
+// console.log('Removing "Pluto" from the end of the planets array.');
+// console.log(planets);
 
-let index = planets.indexOf('Earth');
-console.log('Finding and logging the index of "Earth" in the planets array: ' + index);
+// let index = planets.indexOf('Earth');
+// console.log('Finding and logging the index of "Earth" in the planets array: ' + index);
 
-planets.reverse()
-console.log("Reversing the order of the planets array.");
-console.log(planets);
+// planets.reverse()
+// console.log("Reversing the order of the planets array.");
+// console.log(planets);
 
-planets.sort()
-console.log("Sorting the planets array.");
-console.log(planets);
+// planets.sort()
+// console.log("Sorting the planets array.");
+// console.log(planets);
+
+//============================
+
+const planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+let planetsArray = planetsString.split("|");
+
+/**
+ * TODO:
+ * Convert planetsString to an array, and save it in a variable named
+ * planetsArray.
+ * console.log planetsArray to check your work
+ */
+
+console.log(planetsArray);
+
+/**
+ * TODO:
+ * Create a string with <br> tags between each planet. console.log() your
+ * results. Why might this be useful?
+ */
+let planetsStringBr = planetsArray.join("<br>")
+console.log(planetsStringBr)
