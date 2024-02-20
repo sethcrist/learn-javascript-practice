72c03e50a3b26612ccd83c9eeb57aaca39648f4c
Seth Crist <sethcrist50@gmail.com>
2024-02-20 10:47:18 -0600

    more practice

--- arrays.js
+++ arrays.js
@@ -130,19 +130,62 @@ let names = ["seth", "zack", "morgan", "mark"]
  *  > second([1, 2, 3, 4, 5]) // returns 2
  *  > last([1, 2, 3, 4, 5]) // return 5
  */
-let array1 = [1, 2, 3, 4, 5, 7]
-const first = (arr1) => {
-return array1[0]
-}
-console.log(first(array1))
+// let array1 = [1, 2, 3, 4, 5, 7]
+// const first = (arr1) => {
+// return array1[0]
+// }
+// console.log(first(array1))
 //-----------
-const second = (arr1) => {
-    return array1[1]
-}
-console.log(second(array1))
+// const second = (arr1) => {
+//     return array1[1]
+// }
+// console.log(second(array1))
 //-----------
-const third = (arr1) => {
-    return array1[array1.length - 1]
-}
-console.log(third(array1))
+// const third = (arr1) => {
+//     return array1[array1.length - 1]
+// }
+// console.log(third(array1))
+//=================================================
 
+const planets = [
+    'Mercury',
+    'Venus',
+    'Earth',
+    'Mars',
+    'Jupiter',
+    'Saturn',
+    'Uranus',
+    'Neptune'
+];
+
+/**
+ * TODO:
+ * Read each console log below, and write some javascript code to perform
+ * the step that it describes
+ */
+planets.unshift("Sun")
+console.log('Adding "The Sun" to the beginning of the planets array.');
+console.log(planets);
+
+planets.push("Pluto")
+console.log('Adding "Pluto" to the end of the planets array.');
+console.log(planets);
+
+planets.shift()
+console.log('Removing "The Sun" from the beginning of the planets array.');
+console.log(planets);
+
+planets.pop()
+console.log('Removing "Pluto" from the end of the planets array.');
+console.log(planets);
+
+let index = planets.indexOf('Earth');
+console.log('Finding and logging the index of "Earth" in the planets array: ' + index);
+
+planets.reverse()
+console.log("Reversing the order of the planets array.");
+console.log(planets);
+
+planets.sort()
+console.log("Sorting the planets array.");
+console.log(planets);
