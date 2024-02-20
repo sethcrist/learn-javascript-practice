77093a35409e25c8601624ee38c6f6ed5ba7c300
Seth Crist <sethcrist50@gmail.com>
2024-02-20 10:28:47 -0600

    more practice on arrays

--- arrays.js
+++ arrays.js
@@ -91,38 +91,58 @@ let names = ["seth", "zack", "morgan", "mark"]
  * Create a log statement that will log the number of elements in the names
  * array.
  */
-console.log(names.length)
+// console.log(names.length)
 
 /**
  * TODO:
  * Create log statements that will print each of the names individually by
  * accessing each element's index.
  */
-console.log()
+// console.log(names[0], names[1], names[2], names[3])
 
 /**
  * TODO:
  * Write some code that uses a for loop to log every item in the names
  * array.
  */
+//     for (let i = 0; i < names.length; i++) {
+//         console.log(names[i])
+// }
 
 /**
  * TODO:
  * Refactor your above code to use a `forEach` loop
  */
+// for (let elements of names) {
+//     console.log(names)
+// }
 
 /**
  * TODO:
  * Create the following three functions, each will accept an array and
  * return an an element from it
- * - first: returns the first item in the array
- * - second: returns the second item in the array
- * - last: returns the last item in the array
+ * - first: returns the first element in the array
+ * - second: returns the second element in the array
+ * - last: returns the last element in the array
  *
  * Example:
  *  > first([1, 2, 3, 4, 5]) // returns 1
  *  > second([1, 2, 3, 4, 5]) // returns 2
  *  > last([1, 2, 3, 4, 5]) // return 5
  */
-
+let array1 = [1, 2, 3, 4, 5, 7]
+const first = (arr1) => {
+return array1[0]
+}
+console.log(first(array1))
+//-----------
+const second = (arr1) => {
+    return array1[1]
+}
+console.log(second(array1))
+//-----------
+const third = (arr1) => {
+    return array1[array1.length - 1]
+}
+console.log(third(array1))
 
