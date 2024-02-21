c1c3039a8996fb68261ce6caa2ba462992eceb83
Seth Crist <sethcrist50@gmail.com>
2024-02-21 10:18:02 -0600

    made a correction

--- objects.js
+++ objects.js
@@ -178,66 +178,9 @@
 const person = {
     firstName: "seth",
     lastName: "crist",
-    getData: function () {
+    fullName: function () {
         return `Hello ${this.firstName} ${this.lastName}, I hope you have a great day today`
     }
 }
-console.log(person.getData())
+console.log(person.fullName())
 
-/** TODO:
- * HEB has an offer for the shoppers that buy products amounting to
- * more than $200. If a shopper spends more than $200, they get a 12%
- * discount. Write a JS program, using conditionals, that logs to the
- * browser, how much Ryan, Cameron and George need to pay. We know that
- * Cameron bought $180, Ryan $250 and George $320. Your program will have to
- * display a line with the name of the person, the amount before the
- * discount, the discount, if any, and the amount after the discount.
- *
- * Uncomment the lines below to create an array of objects where each object
- * represents one shopper. Use a foreach loop to iterate through the array,
- * and console.log the relevant messages for each person
- */
-
-// var shoppers = [
-//     {name: 'Cameron', amount: 180},
-//     {name: 'Ryan', amount: 250},
-//     {name: 'George', amount: 320}
-// ];
-
-/** TODO:
- * Create an array of objects that represent books and store it in a
- * variable named `books`. Each object should have a title and an author
- * property. The author property should be an object with properties
- * `firstName` and `lastName`. Be creative and add at least 5 books to the
- * array
- *
- * Example:
- * > console.log(books[0].title) // "The Salmon of Doubt"
- * > console.log(books[0].author.firstName) // "Douglas"
- * > console.log(books[0].author.lastName) // "Adams"
- */
-
-/**
- * TODO:
- * Loop through the books array and output the following information about
- * each book:
- * - the book number (use the index of the book in the array)
- * - the book title
- * - author's full name (first name + last name)
- *
- * Example Console Output:
- *
- *      Book # 1
- *      Title: The Salmon of Doubt
- *      Author: Douglas Adams
- *      ---
- *      Book # 2
- *      Title: Walkaway
- *      Author: Cory Doctorow
- *      ---
- *      Book # 3
- *      Title: A Brief History of Time
- *      Author: Stephen Hawking
- *      ---
- *      ...
- */
